100979cd367d0cd21fca3cf2d55b6d6817981e68
TalkingTem <talkingtem@gmail.com>
2024-09-27 17:47:36 -0500

    add hello.java and README.md

---
+++ Null.java
@@ -0,0 +1,8 @@
+public  class Null {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+    
+}
+//git init(initialze)
+//git add -> gets us to staging area -> git commit -m "description"
